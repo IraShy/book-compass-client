bf230323cda1d9a472cbda14238dd9411f3458ff
IraShy <60783373+IraShy@users.noreply.github.com>
2025-08-07 15:07:20 +1000

    feat(auth): implement logout

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,10 +1,21 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 function Profile() {
+  const navigate = useNavigate();
+  const handleClick = async (e: React.MouseEvent) => {
+    console.log("in handleClick");
+    const response = await axios.post("users/logout");
+    console.log("post request sent");
+    console.log(response);
+    navigate("/");
+  };
   return (
     <div className="container">
       <h2 className="title">Profile</h2>
       <p>Hello user</p>
 
-      <button className="btn" onClick={() => console.log("Logout clicked")}>
+      <button className="btn" onClick={handleClick}>
         Logout
       </button>
     </div>
